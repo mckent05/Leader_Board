0d84618eb1679a2e0b3881d638a9a9c4f00793ab
MCKENT05 <atemitope95@gmail.com>
2021-11-08 18:44:38 +0100

    made little changes

--- src/get_scores.js
+++ src/get_scores.js
@@ -1,16 +1,16 @@
 const createNewScore = (user, score) => {
-    const userScore = { user, score }
-    return fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/r8MdtPvt8QNRAgM5KlJw/scores', {
+  const userScore = { user, score };
+  return fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/r8MdtPvt8QNRAgM5KlJw/scores',
+  {
     method: 'POST',
     body: JSON.stringify(userScore),
     headers: {
         'Content-type': 'application/json; charset=UTF-8',
     },
-})
-}
+    }
+    );
+};
 
-const getAllScores = () => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/r8MdtPvt8QNRAgM5KlJw/scores')
+const getAllScores = () => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/r8MdtPvt8QNRAgM5KlJw/scores');
 
-
-
-export { createNewScore, getAllScores }
+export { createNewScore, getAllScores };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import './style.css';
 import { createNewScore, getAllScores } from './get_scores';
 
 let listContainer = document.querySelector('.scores');
-const form = document.querySelector('.form')
-const name = document.querySelector('.user-name')
-const userScore = document.querySelector('.user-score')
-const alert = document.querySelector('.alert')
-const refresh = document.querySelector('.refresh-btn')
+const form = document.querySelector('.form');
+const name = document.querySelector('.user-name');
+const userScore = document.querySelector('.user-score');
+const alert = document.querySelector('.alert');
+const refresh = document.querySelector('.refresh-btn');
 
 const displayscores = (list) => {
   let display = list.map((item) => `<li><span class='name'>${item.user}</span>:<span class='score'>${item.score}</span></li>`);
@@ -15,43 +15,41 @@ const displayscores = (list) => {
 };
 
 const displayAlert = (message, action) => {
-  alert.innerHTML = message
-  alert.classList.add(`alert-${action}`)
+  alert.innerHTML = message;
+  alert.classList.add(`alert-${action}`);
   setTimeout(() => {
-    alert.innerHTML = ''
-    alert.classList.remove(`alert-${action}`)
-  }, 3000)
-}
+    alert.innerHTML = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 3000);
+};
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const postScore = async() => {
+  e.preventDefault();
+  const postScore = async () => {
     try {
-      let id = await(await createNewScore(name.value, userScore.value)).json()
-      displayAlert(id.result, 'success')
-    } catch(err) {
-      return err
+      let id = await (await createNewScore(name.value, userScore.value)).json();
+      displayAlert(id.result, 'success');
+    } catch (err) {
+      return err;
     }
-  }
-  postScore()
-  name.value = ''
-  userScore.value =''
-})
-
-const getScore = async() => {
-  try {
-    const myScore = await(await getAllScores()).json()
-    displayscores(myScore.result)
-  } catch(err) {
-    return err
-  }
-}
-
-refresh.addEventListener('click', () => {
-  getScore()
-})
-
-window.addEventListener('DOMContentLoaded', () => {
-  getScore()
+  };
+  postScore();
+  form.reset()
 });
 
+const getScore = async () => {
+  try {
+    const myScore = await (await getAllScores()).json();
+    displayscores(myScore.result);
+  } catch (err) {
+    return err;
+  }
+};
+
+refresh.addEventListener('click', () => {
+  getScore();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  getScore();
+});
