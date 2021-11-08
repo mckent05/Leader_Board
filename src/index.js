73b2e52519ebcaf469f06706e3dad6ac617ff5d9
MCKENT05 <atemitope95@gmail.com>
2021-11-08 16:30:31 +0100

    completed leaderboard with api

--- src/index.js
+++ src/index.js
@@ -1,32 +1,57 @@
 import './style.css';
+import { createNewScore, getAllScores } from './get_scores';
 
-const listContainer = document.querySelector('.scores');
-
-const scoreList = [
-  {
-    userName: 'Tope',
-    score: 100,
-  },
-  {
-    userName: 'Samauel',
-    score: 80,
-  },
-  {
-    userName: 'Blessed',
-    score: 70,
-  },
-  {
-    userName: 'Emmanuel',
-    score: 120,
-  },
-];
+let listContainer = document.querySelector('.scores');
+const form = document.querySelector('.form')
+const name = document.querySelector('.user-name')
+const userScore = document.querySelector('.user-score')
+const alert = document.querySelector('.alert')
+const refresh = document.querySelector('.refresh-btn')
 
 const displayscores = (list) => {
-  let display = list.map((item) => `<li><span class='name'>${item.userName}</span>:<span class='score'>${item.score}</span></li>`);
+  let display = list.map((item) => `<li><span class='name'>${item.user}</span>:<span class='score'>${item.score}</span></li>`);
   display = display.join('');
   listContainer.innerHTML = display;
 };
 
+const displayAlert = (message, action) => {
+  alert.innerHTML = message
+  alert.classList.add(`alert-${action}`)
+  setTimeout(() => {
+    alert.innerHTML = ''
+    alert.classList.remove(`alert-${action}`)
+  }, 3000)
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const postScore = async() => {
+    try {
+      let id = await(await createNewScore(name.value, userScore.value)).json()
+      displayAlert(id.result, 'success')
+    } catch(err) {
+      return err
+    }
+  }
+  postScore()
+  name.value = ''
+  userScore.value =''
+})
+
+const getScore = async() => {
+  try {
+    const myScore = await(await getAllScores()).json()
+    displayscores(myScore.result)
+  } catch(err) {
+    return err
+  }
+}
+
+refresh.addEventListener('click', () => {
+  getScore()
+})
+
 window.addEventListener('DOMContentLoaded', () => {
-  displayscores(scoreList);
+  getScore()
 });
+
